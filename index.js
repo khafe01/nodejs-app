ae34971dbeaacc35afb8e155bae74c27e9487295
Ferdouds Khaleque <khafe01@gmail.com>
2019-10-06 08:27:55 -0400

    Adding new code

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 //importing node framework
+
+var nocache = require('no-cache');
+var request = require('superagent');
+var prefix = require('superagent-prefix')('/static');
 var express = require("express");
  
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
+ 
+prefix(request); // Prefixes *all* requests
+ 
+request
+.get('/some-url')
+.use(nocache) // Prevents caching of *only* this request
+.end(function(res){
+    // Do something
+});
 app.get("/", function (req, res) {
  res.send("hey CSP world, this is very beginning of implementing CICD piplines");
 });
